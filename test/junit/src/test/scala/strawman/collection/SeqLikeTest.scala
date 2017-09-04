eaad308ded90ae6681f5685fe5b7a1f3f9f68e4a
Ethan Pronovost <epronovo@caltech.edu>
2017-09-04 09:20:55 -0700

    Add junit tests

--- test/junit/src/test/scala/strawman/collection/SeqLikeTest.scala
+++ test/junit/src/test/scala/strawman/collection/SeqLikeTest.scala
@@ -23,4 +23,14 @@ class SeqLikeTest {
     assertEquals(List(List(1, 2), List(1, 3), List(2, 3)), List(1, 2, 3).combinations(2).toList)
     assertEquals(List(List(1, 2, 3)), List(1, 2, 3).combinations(3).toList)
   }
+  
+  @Test def intersperse(): Unit = {
+    import strawman.collection.immutable.List
+    assertEquals(List(1, 0, 2, 0, 3), List(1, 2, 3).intersperse(0))
+    assertEquals(List(-1, 1, 0, 2, 0, 3, -2), List(1, 2, 3).intersperse(-1, 0, -2))
+    assertEquals(List.empty[Int], List.empty[Int].intersperse(0))
+    assertEquals(List(1, 2), List.empty[Int].intersperse(1, 0, 2))
+    assertEquals(List(1), List(1).intersperse(0))
+    assertEquals(List(0, 1, 2), List(1).intersperse(0, 5, 2))
+  }
 }
